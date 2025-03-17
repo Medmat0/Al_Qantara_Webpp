c1bc7e86b9245d1db474ff8c6e8609b2f0a45c73
Ark1ight <benoi1.suy@gmail.com>
2025-03-17 15:22:12 +0100

    base Impl of nav bar

---
+++ al-qantara-front-v1/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './components/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  template: `
+    <app-header></app-header>
+    <app-home></app-home>
+
+  `,
+  styles: [],
+  standalone: true
+})
+export class AppComponent {
+  title = 'al-qantara-front-v1-root';
+}
