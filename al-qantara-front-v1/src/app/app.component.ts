50c6c698524b038033069675f7f0bcec75b58933
Ark1ight <benoi1.suy@gmail.com>
2025-03-25 10:41:33 +0100

    changed app to respect more SPA and added reset-password.component

--- al-qantara-front-v1/src/app/app.component.ts
+++ al-qantara-front-v1/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {HeaderComponent} from "./home-components/header/header.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   template: `
     <main>
+      <app-header></app-header>
       <router-outlet></router-outlet>
     </main>
 
--- al-qantara-front-v1/src/app/home/home.component.ts
+++ al-qantara-front-v1/src/app/home/home.component.ts
@@ -3,9 +3,8 @@ import { HeaderComponent } from '../home-components/header/header.component';
 import {HomeContentComponent} from '../home-components/home-content/home-content.component';
 
 @Component({
-  selector: 'app-home-header',
+  selector: 'app-main-home',
   template: `
-    <app-header></app-header>
     <app-home-content></app-home-content>
   `,
   standalone: true,
