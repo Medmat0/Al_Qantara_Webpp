61ab1229c5bdb7bb31d11aee6ecbd7f031785c3d
Ark1ight <benoi1.suy@gmail.com>
2025-04-03 16:14:11 +0200

    continued base Impl for auth https methods

--- al-qantara-front-v1/src/app/member/services/auth.service.ts
+++ al-qantara-front-v1/src/app/member/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { User } from '../models/user';
+import {Observable, tap} from 'rxjs';
 import { API_URL } from '../utils/config';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +12,31 @@ import { API_URL } from '../utils/config';
 export class AuthService {
   private apiUrl = `${API_URL}/auth`; // URL de l'API d'authentification
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private  router:Router
+  ) {}
 
 
   login(email: string, password: string): Observable<any> {
     const body = { email, password };
-    return this.http.post(`${this.apiUrl}/login`, body);
+    return this.http.post(`${this.apiUrl}/login`, body).pipe(
+      tap((response: any) => {
+        if (response && response.token) {
+          // Stocker le token JWT dans le stockage local
+          localStorage.setItem('auth_token', response.token);
+          console.log('Login successful:', response);
+        }
+      })
+    );
   }
 
+  logout(): void {
+    // Supprimer le token JWT du stockage local
+    localStorage.removeItem('auth_token');
+    console.log('Logout successful');
+    this.router.navigate(['']).then(r => console.log(r));
 
+  }
 
 }
--- al-qantara-front-v1/src/app/pages/auth/components/login/login.component.ts
+++ al-qantara-front-v1/src/app/pages/auth/components/login/login.component.ts
@@ -1,25 +1,37 @@
 import {Component, inject} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../../../member/services/auth.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-login',
   imports: [
     RouterLink,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CommonModule
   ],
   templateUrl: './login.component.html',
   standalone: true,
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  fb = inject(FormBuilder);
+  // Déclaration des dépendances
+  fb : FormBuilder = inject(FormBuilder);
+  authService = inject(AuthService);
+
   loginForm = this.fb.group({
-    email: ['',Validators.required],
+    email: ['',Validators.required, Validators.email],
     password: ['',Validators.required]
   });
 
   onSubmit() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email')?.value;
+      const password = this.loginForm.get('password')?.value;
+      console.log('Login form submitted:', this.loginForm.value);
+
+    }
 
   }
 }
