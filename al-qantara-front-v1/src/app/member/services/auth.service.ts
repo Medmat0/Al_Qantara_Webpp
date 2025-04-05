0e99ca5212fa1989b2bfd82e1d1dd8a17083c4ed
Ark1ight <benoi1.suy@gmail.com>
2025-04-05 14:23:20 +0200

    added behavior if user connected or not

--- al-qantara-front-v1/src/app/member/services/auth.service.ts
+++ al-qantara-front-v1/src/app/member/services/auth.service.ts
@@ -1,42 +1,53 @@
+// src/app/member/services/auth.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable, tap} from 'rxjs';
+import { Observable, tap, BehaviorSubject, catchError, throwError } from 'rxjs';
 import { API_URL } from '../utils/config';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-
-
 export class AuthService {
-  private apiUrl = `${API_URL}/auth`; // URL de l'API d'authentification
+  private apiUrl = `${API_URL}/auth`; // URL of the authentication API
+  private authStatusSubject = new BehaviorSubject<boolean>(this.isAuthenticated());
+  authStatus$ = this.authStatusSubject.asObservable();
 
   constructor(
     private http: HttpClient,
-    private  router:Router
+    private router: Router
   ) {}
-
 
   login(email: string | null | undefined, password: string | null | undefined): Observable<any> {
     const body = { email, password };
     return this.http.post(`${this.apiUrl}/login`, body).pipe(
-      tap((response:any) => {
+      tap((response: any) => {
         if (response && response.token) {
-          // Stocker le token JWT dans le stockage local
+          // Store the JWT token and user information in local storage
           localStorage.setItem('auth_token', response.token);
+          localStorage.setItem('user', JSON.stringify(response.utilisateur));
           console.log('Login successful:', response);
+          this.authStatusSubject.next(true);
         }
+      }),
+      catchError((error) => {
+        console.error('Login failed', error);
+        return throwError(() => new Error('Login failed'));
       })
     );
   }
 
   logout(): void {
-    // Supprimer le token JWT du stockage local
+    // Remove the JWT token and user information from local storage
     localStorage.removeItem('auth_token');
+    localStorage.removeItem('user');
     console.log('Logout successful');
+    this.authStatusSubject.next(false);
     this.router.navigate(['']).then(r => console.log(r));
-
   }
 
+  //used for dynamic front end like login register and logout buttons in navbar
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('auth_token');
+  }
 }
