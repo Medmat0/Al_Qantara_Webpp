1b831ea016bf9fa883e675de73b6cf395146b8d8
Omar EL BARAKA <elbarakaomar99@hotmail.com>
2025-04-06 21:14:05 +0200

    add revue route

--- al-qantara-front-v1/src/app/app.routes.ts
+++ al-qantara-front-v1/src/app/app.routes.ts
@@ -6,8 +6,7 @@ export const routes: Routes = [
   },
   {path: 'profile', loadChildren: () => import('./pages/profile/profile.routes').then(m => m.default)
   },
-  {path: 'profile', loadChildren: () => import('./pages/profile/profile.routes').then(m => m.default)
-  },
+  { path: 'revues', loadChildren: () => import('./pages/revues/revues.routes').then(m => m.default) },
   {path: 'articles', loadChildren: () => import('./pages/articles/articles.routes').then(m => m.default)},
   {path: 'contacts', loadChildren: () => import('./pages/contacts/contacts.routes').then(m => m.default)},
   {
---
+++ al-qantara-front-v1/src/app/pages/revues/revues.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { AddRevueComponent } from './components/addRevue/add-revue.component';
+
+export default [
+  { path: 'add', component: AddRevueComponent }
+] as Routes;
