0e1ab391c6579d2683ab3f70067460c893fb4d3a
Medmat0 <yassineelmatrour@gmail.com>
2025-04-12 17:33:35 +0200

    adding get revue by id

---
+++ Back_End/src/controllers/revues/getRevuesByid.js
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+/**
+ * @desc    Obtenir une revue par ID (accessible à tous authentifiés)
+ * @method  GET
+ * @route   /revues/:id
+ */
+const getRevueById = async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const revue = await prisma.revue.findUnique({
+        where: { id: parseInt(id) },
+        select: {
+          titre: true,
+          id: true,
+          fichier: true,
+          datePublication: true,
+          createdBy: true,
+        },
+      });
+  
+      if (!revue) {
+        return res.status(404).json({ message: "Revue non trouvée." });
+      }
+  
+      res.status(200).json(revue);
+    } catch (error) {
+      res.status(500).json({ message: "Erreur lors de la récupération de la revue.", error: error.message });
+    }
+  };
+  
+  export { getRevueById };
+  
--- Back_End/src/routes/revues.routes.js
+++ Back_End/src/routes/revues.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addRevue, deleteRevue, getRevues } from "../controllers/revues/Revues.index.js";
+import { addRevue, deleteRevue, getRevues, getRevueById } from "../controllers/revues/Revues.index.js";
 import { isAdmin, authMiddleware } from "../middleware/auth.middleware.js"
 import {upload} from "../middleware/storage.middleware.js";
 
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post("/add", isAdmin ,upload.single("document"), addRevue);
 router.delete("/delete/:id", isAdmin ,deleteRevue);
 router.get("/",authMiddleware,getRevues);
+router.get("/:id", authMiddleware, getRevueById); 
 
 
 
