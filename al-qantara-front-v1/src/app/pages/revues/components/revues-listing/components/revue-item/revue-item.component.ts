96fe7415cd4ca018dcf9debaf57fc561d05ad3fe
Ark1ight <benoi1.suy@gmail.com>
2025-04-13 11:15:10 +0200

    created revue item

---
+++ al-qantara-front-v1/src/app/pages/revues/components/revues-listing/components/revue-item/revue-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-revue-item',
+  imports: [],
+  templateUrl: './revue-item.component.html',
+  standalone: true,
+  styleUrl: './revue-item.component.scss'
+})
+export class RevueItemComponent {
+
+}
