08c528531bb678ab5928939089f8a82469ac8534
Ark1ight <benoi1.suy@gmail.com>
2025-04-22 13:50:30 +0200

    implemented basic guards for admin pages

--- al-qantara-front-v1/src/app/pages/admin/admin.routes.ts
+++ al-qantara-front-v1/src/app/pages/admin/admin.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { AdminHomeComponent } from './components/admin-home/admin-home.component';
+import {AdminGuard} from '../../member/guards/admin.guard';
 
 export default [
   {
     path: '',
+    canActivate:[AdminGuard],
     component: AdminHomeComponent
+
   }
 ] as Routes;
