83653eba29d16007db3e50abda9422f8ee95390d
Ark1ight <benoi1.suy@gmail.com>
2025-04-24 11:15:46 +0200

    added route to revues and modified revue description css

--- al-qantara-front-v1/src/app/pages/home/components/home-content/home-content.component.ts
+++ al-qantara-front-v1/src/app/pages/home/components/home-content/home-content.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home-content',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './home-content.component.html',
   standalone: true,
   styleUrl: './home-content.component.scss'
