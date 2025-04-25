147d23e74d866643899764ce90221c37c319ac15
Medmat0 <yassineelmatrour@gmail.com>
2025-04-25 21:41:22 +0200

    fixning some probs v2

--- Back_End/index.js
+++ Back_End/index.js
@@ -35,7 +35,7 @@ app.use(
 
 app.use(cors({
   // Autorise les requêtes CORS seulement depuis le frontend
-  origin: "https://alqantara.vercel.app/",
+  origin: "https://alqantara.vercel.app",
   credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
