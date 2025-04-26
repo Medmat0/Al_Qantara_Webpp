6081aeec012f8cfa824413d453a55cdd58ff602d
Ark1ight <benoi1.suy@gmail.com>
2025-04-26 19:35:44 +0200

    corrected error in annee var

--- al-qantara-front-v1/src/app/pages/revues/components/addRevue/add-revue.component.ts
+++ al-qantara-front-v1/src/app/pages/revues/components/addRevue/add-revue.component.ts
@@ -20,7 +20,7 @@ export class AddRevueComponent {
       titre: ['', Validators.required],
       description: ['', Validators.required],
       mois: ['', Validators.required],
-      annee: ['', [Validators.pattern(/^\d{4}$/)]],
+      annee: ['', [Validators.required,Validators.pattern(/^\d{4}$/)]],
       document: [null, Validators.required]
     });
   }
