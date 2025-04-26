609df7bf64742aea85d494b780808511f54dfb1f
Ark1ight <benoi1.suy@gmail.com>
2025-04-26 19:12:32 +0200

    modified revue model

--- al-qantara-front-v1/src/app/shared/models/revue.ts
+++ al-qantara-front-v1/src/app/shared/models/revue.ts
@@ -1,9 +1,14 @@
 export class Revue{
     constructor(
-        public titre: string,
         public id: number,
+        public titre: string,
+        public description: string,
+        public mois: string,
+        public annee: string,
         public fichier: string ,
         public datePublication: string,
+        public nombreVues: number,
+        public nombreTelechargements: number,
         public createdBy:number
 
 
