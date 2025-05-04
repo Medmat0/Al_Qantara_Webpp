adcc24f72f10294138e74d79cbf80df4f4e10495
Ark1ight <benoi1.suy@gmail.com>
2025-05-04 14:02:02 +0200

    added forgotten method

--- Back_End/index.js
+++ Back_End/index.js
@@ -37,7 +37,7 @@ app.use(cors({
   // Autorise les requêtes CORS seulement depuis le frontend
   origin: "https://alqantara.vercel.app",
   credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(bodyParser.json()); 
