ee5736130d8bc9c0234d8cdbe0aa9dc84fb5b80a
Ark1ight <benoi1.suy@gmail.com>
2025-05-10 14:01:52 +0200

    added forgotten event model

---
+++ al-qantara-front-v1/src/app/shared/models/event.ts
@@ -0,0 +1,16 @@
+export class Event {
+    constructor(
+        public id: number,
+        public titre: string,
+        public description: string,
+        public image: string,
+        public nombrePlaces: number,
+        public tags: [string],
+        public dateDebut: Date,
+        public dateFin: Date,
+        public lieu: string,
+        public datePublication: Date,
+        public createdBy:number
+    ) {
+    }
+}
