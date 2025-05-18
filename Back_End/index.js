0a11a33384de8599726eb8ae623400f6b254bf1a
Medmat0 <yassineelmatrour@gmail.com>
2025-05-18 19:53:19 +0200

    changing the url front end

--- Back_End/index.js
+++ Back_End/index.js
@@ -35,7 +35,7 @@ app.use(
 
 app.use(cors({
   // Autorise les requêtes CORS seulement depuis le frontend
-  origin: "https://alqantara.vercel.app",
+  origin: process.env.FRONT_URL,
   credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
