a5f1523ca27e4a2c78d1010b514b1ddf7a117d74
Ark1ight <benoi1.suy@gmail.com>
2025-05-23 15:05:48 +0200

    changed event list css, added interceptor for Invalid or Expired AccessToken

--- al-qantara-front-v1/src/app/pages/events/event-listing/event-item/event-item.component.ts
+++ al-qantara-front-v1/src/app/pages/events/event-listing/event-item/event-item.component.ts
@@ -1,14 +1,15 @@
-import {Component, Input} from '@angular/core';
-import {Evenement} from '../../../../member/models/evenement';
+import { Component, Input } from '@angular/core';
+import { Evenement } from '../../../../member/models/evenement';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-event-item',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './event-item.component.html',
   standalone: true,
-  styleUrl: './event-item.component.scss'
+  styleUrl: './event-item.component.scss',
+  providers: [DatePipe]
 })
 export class EventItemComponent {
   @Input() event!: Evenement;
-
 }
