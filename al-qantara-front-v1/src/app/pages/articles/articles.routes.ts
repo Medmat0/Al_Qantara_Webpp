f289f2029093e0ef22f361078038c7515f6d6a48
Omar EL BARAKA <elbarakaomar99@hotmail.com>
2025-07-06 16:27:00 +0200

    lire article

--- al-qantara-front-v1/src/app/pages/articles/articles.routes.ts
+++ al-qantara-front-v1/src/app/pages/articles/articles.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { ArticlesListingComponent } from './components/articles-listing/articles-listing.component';
+import { ArticleComponent } from './components/article/article.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ArticlesListingComponent
+  },
+  {
+    path: ':id',
+    component: ArticleComponent
   }
 ];
 
