f601024c77051397d89a63e4be2512ac7a4750a0
Medmat0 <yassineelmatrour@gmail.com>
2025-07-06 02:22:30 +0200

    add asso

--- Back_End/index.js
+++ Back_End/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import associationRoutes from './src/routes/association.routes.js';
 import { createServer } from "http";
 import { Server } from "socket.io";
 import authRoutes from "./src/routes/auth.routes.js";
@@ -185,6 +186,7 @@ app.use("/messages", messagerieRoutes);
 app.use("/newsletter", newsletterRoutes);
 app.use("/adhesion", adhesionRoutes);
 app.use("/communities", communityRoutes);
+app.use("/associations", associationRoutes);
 
 // Démarrer le serveur avec Socket.IO
 httpServer.listen(PORT, () => {
