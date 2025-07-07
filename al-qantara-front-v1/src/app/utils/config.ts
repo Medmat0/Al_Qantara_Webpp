f664864252b8b367eac20e09ac03da610b897ec4
Ark1ight <benoi1.suy@gmail.com>
2025-07-07 11:35:01 +0200

    changed back url

--- al-qantara-front-v1/src/app/utils/config.ts
+++ al-qantara-front-v1/src/app/utils/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3000"; // Base URL of the API
-export const CV_WEB_SERVICE_URL = "http://localhost:8000"; 
+export const API_URL = "https://al-qantara-webpp.onrender.com"; // Base URL of the API
+export const CV_WEB_SERVICE_URL = "http://localhost:8000"; // To change when hosted
