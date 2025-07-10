cbdf4656204636e85a02ed8c1fca50cb11ab11d8
Medmat0 <yassineelmatrour@gmail.com>
2025-07-10 02:52:09 +0200

    pushing new url to test

--- al-qantara-front-v1/src/app/utils/config.ts
+++ al-qantara-front-v1/src/app/utils/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://al-qantara-webpp.onrender.com"; // Base URL of the API
+export const API_URL = "https://defensive-chrissy-alqantaraasas-25adca48.koyeb.app" //"https://al-qantara-webpp.onrender.com"; // Base URL of the API
 export const CV_WEB_SERVICE_URL = "https://crooked-cristin-alqantara-76ea917d.koyeb.app"; // To change when hosted
