1e1939826075d286d7114e4bb0f98fe370d22656
Medmat0 <yassineelmatrour@gmail.com>
2025-07-19 13:13:28 +0200

    fixing register V&

--- Back_End/src/controllers/authentification/registerUser.js
+++ Back_End/src/controllers/authentification/registerUser.js
@@ -35,7 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
       telephone,
       role: userRole,
       dateInscription: new Date(), 
-      statut: "waiting_verification",
+      statut: "ACTIF",
 
     },
   });
